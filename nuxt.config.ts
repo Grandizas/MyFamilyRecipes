27a3483bc3639f601a9d74e85f602b70c9d6963c
Grazvydas Z <grazvydas.cherry@gmail.com>
2024-12-21 09:59:19 +0200

    New: Add SCSS structure and configuration for styling.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '@/assets/scss/main.scss' // path to your main SCSS file
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/abstracts/_variables.scss" as *;'
+        }
+      }
+    }
+  }
 })
